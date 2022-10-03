0a8f0e1971981cd04807b996414cb6257930edec
TalysonSoares <talysonsoares40@gmail.com>
2022-10-03 10:52:25 -0300

    fazendo o filtrar funcionar

--- filtrar.js
+++ filtrar.js
@@ -1,3 +1,19 @@
 function filtrar() {
-    console.log(input_busca.value)
+    let expressao= input_busca.value //valor que o usuario digitous
+
+
+    //pegando todas as linhas da tabela
+    let linhas = tabela_compras.getElementsByTagName('tr'); 
+
+    for (let posicao in linhas) {
+        if (isNaN(posicao) ) {
+            continue;
+        }
+
+        if( linhas[posicao].innerText.includes(expressao) ) {
+            linhas[posicao].style.display = '';
+        } else {
+            linhas[posicao].style.display = 'none';
+        }
+    }
 }
